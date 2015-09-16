d52a5e60a8b3650da41e79f615514d09db4d6a72
Doug Walter <doug@kosmojo.com>
2015-09-16 16:45:38 -0600

    tweet, tweetbox, tweetlist

--- public/app.js
+++ public/app.js
@@ -1,8 +1,44 @@
+var Tweet = React.createClass({
+	render: function(){
+		return (
+			<div className="tweet">
+				<h2 className="tweetAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+				</div>
+			);
+	}
+});
+
+var TweetList = React.createClass({
+  render: function() {
+    return (
+      <div className="tweetList">
+        <h1 Tweet author="Just_be_dougin"> This guys an author</h1>
+        <h1 Tweet author="Don't be douging"> This guys another author </h1> 
+      </div>
+    );
+  }
+});
+
+var TweetForm = React.createClass({
+  render: function() {
+    return (
+      <div className="tweetForm">
+        Hello, world! I am a TweetForm.
+      </div>
+    );
+  }
+});
+
 var TweetBox = React.createClass({
 	render: function(){
 		return(
 			<div clasName="tweetBox">
-				Hello, world! I am a TweetBox.
+				<h1> Tweets </h1>
+				<TweetList />
+				<TweetForm />
 			</div>
 			);
 	}
