1fa6c79ab883f238d6f28e0e28a09c896e8a5048
Doug Walter <doug@kosmojo.com>
2015-09-18 13:10:47 -0600

    add user image

--- public/app.js
+++ public/app.js
@@ -1,12 +1,21 @@
 var Tweet = React.createClass({
   render: function(){
     return (
-      <div className="tweet">
+      <div className="panel panel-default">
+      <div className="panel-heading ">
+        <div className="row">
+        <img src={this.props.tweet.user.profile_image_url} className="pull-right my-image img-thumbnail"/>
+
         <h2 className="twitterHandle">
-          <p>{'@' + this.props.tweet.user.screen_name}</p>
-          <p>{this.props.tweet.text}</p>
+          <p panel-title className="pull-left">{'@' + this.props.tweet.user.screen_name}</p>
         </h2>
         </div>
+      </div>
+      <div className="panel-body">
+          <h2>{this.props.tweet.text}</h2>
+      </div>
+
+      </div>
       );
   }
 });
